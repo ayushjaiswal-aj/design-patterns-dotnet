0fd7c1fd18af7bcd7961fceab2bae7106158a267
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-04-11 12:08:52 +0530

    Adds Singleton Design Pattern - naive and thread-safe

---
+++ DesignPattersInDotNet/CreationalPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.CreationalPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                return new Singleton();
+            }
+            return _instance;
+        }
+
+        //add business login if any
+    }
+}
---
+++ DesignPattersInDotNet/CreationalPattern/ThreadSafeSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.CreationalPattern
+{
+    public sealed class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton _instance;
+
+        private ThreadSafeSingleton() { }
+
+        private static readonly object _lock = new object();
+
+        public static ThreadSafeSingleton GetInstance()
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new ThreadSafeSingleton();
+                }
+            }
+            return _instance;
+        }
+    }
+}

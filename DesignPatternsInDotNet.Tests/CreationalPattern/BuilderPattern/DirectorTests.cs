85d41ac03eaf51886bdd9493818cd0462d0a5100
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-04-12 14:46:50 +0530

    Adds test for builder pattern director

---
+++ DesignPatternsInDotNet.Tests/CreationalPattern/BuilderPattern/DirectorTests.cs
@@ -0,0 +1,42 @@
+﻿using DesignPattersInDotNet.CreationalPattern.BuilderPattern;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInDotNet.Tests.CreationalPattern.BuilderPattern
+{
+    [TestFixture]
+    public class DirectorTests
+    {
+        [Test]
+        public void BuildMinimalViableProduct_WhenCalled_ShouldReturnProductWithPartA()
+        {
+            Director director = new Director();
+            ConcreteBuilder builder = new ConcreteBuilder();
+            director.Builder = builder;
+            string expectedString = "Product parts: PartA1\n";
+
+            director.BuildMinimalViableProduct();
+
+            string actualString = builder.GetProduct().ListParts();
+            Assert.That(actualString, Is.EqualTo(expectedString));
+        }
+
+        [Test]
+        public void BuildFullFeaturedProduct_WhenCalled_ShouldReturnProductWithAllParts()
+        {
+            Director director = new Director();
+            ConcreteBuilder builder = new ConcreteBuilder();
+            director.Builder = builder;
+            string expectedString = "Product parts: PartA1, PartB1, PartC1\n";
+
+            director.BuildFullFeaturedProduct();
+
+            string actualString = builder.GetProduct().ListParts();
+            Assert.That(actualString, Is.EqualTo(expectedString));
+        }
+    }
+}

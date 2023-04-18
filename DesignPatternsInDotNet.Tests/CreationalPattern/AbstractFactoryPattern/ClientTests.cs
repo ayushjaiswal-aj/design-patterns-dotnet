3b957e76ba8f4d10f4e2e0386950a09db026d36c
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-04-18 12:52:24 +0530

    Adds unit tests for abstract factory pattern client

---
+++ DesignPatternsInDotNet.Tests/CreationalPattern/AbstractFactoryPattern/ClientTests.cs
@@ -0,0 +1,34 @@
+﻿using DesignPattersInDotNet.CreationalPattern.AbstractFactoryPattern;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInDotNet.Tests.CreationalPattern.AbstractFactoryPattern
+{
+    [TestFixture]
+    public class ClientTests
+    {
+        [Test]
+        public void ClientMethod_WhenCalledWithConcreteFactory1_ShouldReturnCorrectString()
+        {
+            ConcreteFactory1 factory = new ();
+            String expectedString = "The result of the product B1.The result of the B1 collaborating with the (The result of the product A1.)";
+            String actualString = Client.ClientMethod(factory);
+
+            Assert.That(actualString, Is.EqualTo(expectedString));
+        }
+
+        [Test]
+        public void ClientMethod_WhenCalledWithConcreteFactory2_ShouldReturnCorrectString()
+        {
+            ConcreteFactory2 factory = new ();
+            String expectedString = "The result of the product B2.The result of the B2 collaborating with the (The result of the product A2.)";
+            String actualString = Client.ClientMethod(factory);
+
+            Assert.That(actualString, Is.EqualTo(expectedString));
+        }
+    }
+}
---
+++ DesignPattersInDotNet/CreationalPattern/AbstractFactoryPattern/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.CreationalPattern.AbstractFactoryPattern
+{
+    public class Client
+    {
+        public static String ClientMethod(IAbstractFactory factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+
+            return productB.UsefulFunctionB() + productB.AnotherUsefulFunctionB(productA);
+        }
+    }
+}

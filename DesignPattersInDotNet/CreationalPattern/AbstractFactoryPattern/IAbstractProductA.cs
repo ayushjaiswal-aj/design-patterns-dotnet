b52003e22872296dba2599a94681b5d2a00d9b28
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-04-18 11:24:27 +0530

    Adds abstract factory pattern example

---
+++ DesignPattersInDotNet/CreationalPattern/AbstractFactoryPattern/IAbstractProductA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.CreationalPattern.AbstractFactoryPattern
+{
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+}
---
+++ DesignPattersInDotNet/CreationalPattern/AbstractFactoryPattern/IAbstractProductB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.CreationalPattern.AbstractFactoryPattern
+{
+    public interface IAbstractProductB
+    {
+        string UsefulFunctionB();
+
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+}

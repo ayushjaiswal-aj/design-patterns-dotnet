d5cc541fd6cee05db64fb3696e6e3ec9469cd05f
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-04-20 10:20:33 +0530

    Adds Observer Pattern (Behavioral)

---
+++ DesignPattersInDotNet/BehavioralPattern/ObserverPattern/ConcreteObserverB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattersInDotNet.BehavioralPattern
+{
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if (((Subject)subject).State == 0 || ((Subject)subject).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+}
